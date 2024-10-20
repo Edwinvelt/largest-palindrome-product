dcf1e7297efb7d5e0165a9d3a4c154fd66ba6e32
Edwin <Edwin.v.velthoven@gmail.com>
2024-10-20 17:07:09 +0200

    Fixed function not calculating every possible option

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,31 @@
 // Find the largest palindrome made from the product of two 3-digit numbers.
 
 fn main() {
-    let mut num1: u32 = 0;
-    let mut num2: u32 = 0;
+    let mut num1: u32 = 1;
+    let mut num2: u32 = 1;
 
     let mut palindromic_list: Vec<u32> = Vec::new();
 
-    while num1 <= 999 && num2 <= 999 {
+    while num1 <= 999 || num2 <= 999 {
       let result: u32 = num1 * num2;
 
       // check if palindromic
       if flip_number(result) == result {
         palindromic_list.push(result);
-        println!("{:?} is a palindromic number", result);
       }
-
       // update nums
-      if num1 > num2 {
-        num2 += 1;
-      }
-      else {
+
+      if num1 <= 999 {
         num1 += 1;
       }
-      
+      else {
+          num1 = 1;
+          num2 += 1;
+      }
     }
+    palindromic_list.sort();
+    println!("{:?}", palindromic_list);
+
 }
 
 fn flip_number(n: u32) -> u32 {
